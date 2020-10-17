d14b3035ceaa13e7381028cd17192557be6fdda5
Blazej Gracz <gracz.blazej@gmail.com>
2020-10-17 19:37:22 +0200

    Pierwsza wersja

---
+++ one.js
@@ -0,0 +1,9 @@
+var today = new Date().getTime();
+
+var partyTime = new Date ("Jun 19, 2021 15:00:00")getTime();
+
+var timeLeft = partyTime - today;
+
+if (timeLeft < 0){
+  alert("be ready for all night party!");
+};
